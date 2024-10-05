f360361eb0527a47e79ebc86f0fdd40aeb01bc9b
lutfiyasantika <santikalutfiya@gmail.com>
2024-10-05 11:37:17 +0700

    Add files via upload

---
+++ tugas 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ main() {
+	char* NAMA = "Lutfiya Santika";
+	char* JURUSAN = "Informatika";
+	char* NIM = "2441023";
+	
+	printf ("NAMA \t\t: %s\n", NAMA);
+	printf ("JURUSAN \t: %s\n", JURUSAN);
+	printf ("NIM \t\t: %s\n", NIM);
+	
+	return 0;
+}
---
+++ tugas 2.cpp
@@ -0,0 +1,11 @@
+   #include <stdio.h>
+   
+   main (){
+   	int panjang = 10;
+	int lebar = 7;
+	int luas;
+	
+	luas = panjang * lebar;
+	
+	printf ("panjang = %d cm \n lebar = %d cm \n\n luas persegi panjang adalah %d cm", panjang, lebar, luas);
+   }
